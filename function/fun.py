a7d01e8c6ec2712a5f351f84ffcc143a4c5ba5a4
uttam9721 <uttammaurya377@gmail.com>
2026-05-25 22:16:07 +0530

    Learning

--- function/fun.py
+++ function/fun.py
@@ -1,5 +1,35 @@
-def uttam(num,name):
-    print(f"hello {name}, you are {num} years old")
+# # def uttam(num,name):
+# #     print(f"hello {name}, you are {num} years old")
 
-uttam(22,"uttam")
-uttam(23,"maurya")
+# # uttam(22,"uttam")
+# # uttam(23,"maurya")
+
+
+
+# def validate_input():
+#     print("Validating the use info")
+
+# def save_to_db():
+#     print("Saving the user info to the database")
+
+
+# def register_user():
+#     validate_input()
+#     save_to_db()
+#     print("Registering the user")
+
+# register_user()
+
+
+
+
+# returned values
+# def calculate_bill(cups,price_per_cup):
+#     total = cups * price_per_cup
+#     return total
+# bill_amount = calculate_bill(3,5)
+# print(f"The total bill amount is: {bill_amount}")
+
+
+
+ 
